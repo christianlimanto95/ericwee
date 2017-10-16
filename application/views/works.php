bb366bbe9bf546df9e9e50a26d4ba599c7a24c7c
Christian - PC <christianlimanto95@gmail.com>
2017-10-16 09:36:39 +0700

    works

--- application/views/works.php
+++ application/views/works.php
@@ -8,4 +8,39 @@
             </div>
         </div>
     </div>
+    <div class="section section-2">
+        <div class="selected-works-title" data-anim="fade-anim"><div data-content-type="text">SELECTED WORKS</div></div>
+        <div class="selected-works-container">
+            <div class="selected-works-item selected-works-item-1" style="background-image: url(assets/images/1.jpg);"></div>
+            <div class="selected-works-item selected-works-item-2" style="background-image: url(assets/images/2.jpg);"></div>
+            <div class="selected-works-item selected-works-item-3" style="background-image: url(assets/images/3.jpg);"></div>
+            <div class="selected-works-item selected-works-item-4" style="background-image: url(assets/images/4.jpg);"></div>
+            <div class="selected-works-item selected-works-item-5" style="background-image: url(assets/images/5.jpg);"></div>
+            <div class="selected-works-item selected-works-item-6" style="background-image: url(assets/images/1.jpg);"></div>
+            <div class="selected-works-item selected-works-item-7" style="background-image: url(assets/images/2.jpg);"></div>
+            <div class="selected-works-item selected-works-item-8" style="background-image: url(assets/images/3.jpg);"></div>
+            <div class="selected-works-item selected-works-item-9" style="background-image: url(assets/images/4.jpg);"></div>
+        </div>
+        <div class="archived-works-title"><div data-content-type="text">ARCHIVED WORKS</div></div>
+        <div class="archived-works-container">
+            <div class="archived-works-item archived-works-item-1" style="background-image: url(assets/images/1.jpg);"></div>
+            <div class="archived-works-item archived-works-item-2" style="background-image: url(assets/images/2.jpg);"></div>
+            <div class="archived-works-item archived-works-item-3" style="background-image: url(assets/images/3.jpg);"></div>
+            <div class="archived-works-item archived-works-item-4" style="background-image: url(assets/images/4.jpg);"></div>
+            <div class="archived-works-item archived-works-item-5" style="background-image: url(assets/images/5.jpg);"></div>
+            <div class="archived-works-item archived-works-item-6" style="background-image: url(assets/images/1.jpg);"></div>
+            <div class="archived-works-item archived-works-item-7" style="background-image: url(assets/images/2.jpg);"></div>
+            <div class="archived-works-item archived-works-item-8" style="background-image: url(assets/images/3.jpg);"></div>
+        </div>
+    </div>
+    <div class="section section-3">
+        <div class="section-3-center">
+            <div class="section-3-contact-me" data-content-type="text">CONTACT ME</div>
+            <div class="social-media">
+                <a href="https://www.instagram.com/ericweephoto/" target="_blank" class="social-media-item" data-content-type="text"><div class="social-media-item-icon" style="background-image: url('assets/icons/instagram.png');"></div>@ericweephoto</a>
+                <div class="social-media-item" data-content-type="text"><div class="social-media-item-icon" style="background-image: url('assets/icons/line.png');"></div>@ericwicak</div>
+                <a href="tel:+6285230515511" class="social-media-item" data-content-type="text"><div class="social-media-item-icon" style="background-image: url('assets/icons/whatsapp.png');"></div>0852 3051 5511</a>
+            </div>
+        </div>
+    </div>
 </div>
--- assets/js/works.js
+++ assets/js/works.js
@@ -1,5 +1,81 @@
+var container, section1, section2, section3;
+var checkScrollDown = true, checkScrollUp = false;
+var vh100, vh33;
+var selectedWorksThreshold, archivedWorksThreshold;
+
 $(function() {
+    container = $(".container");
+    section1 = $(".section-1");
+    section2 = $(".section-2");
+    section3 = $(".section-3");
+    vh100 =  parseInt(section1.css("height"));
+    vh33 = vh100 / 3;
+    section3Threshold = section2.offset().top + parseInt(section2.css("height")) - vh100 / 2;
+
+    selectedWorksThreshold = $(".selected-works-title").offset().top - vh33 * 2;
+    archivedWorksThreshold = $(".archived-works-title").offset().top - vh33 * 2;
+
     $("body").on("allLoaded", function() {
         $(".section-1").addClass("show");
     });
+
+    $(".section-1-works-container").on("click", function() {
+        $(".section-2").velocity("scroll", {
+            container: $(".container"),
+            duration: 500
+        });
+    });
+
+    container.on("scroll", checkSection2ScrollDown);
+    container.on("scroll", selectedWorksShow);
 });
+
+function selectedWorksShow() {
+    if (container.scrollTop() >= selectedWorksThreshold) {
+        $(".selected-works-title").addClass("show");
+        container.off("scroll", selectedWorksShow);
+    }
+}
+
+function checkSection2ScrollDown() {
+    if (checkScrollDown) {
+        if (container.scrollTop() >= section3Threshold) {
+            sectionFooter = true;
+            checkScrollDown = false;
+            checkScrollUp = true;
+            section3.addClass("show");
+            section3.velocity({
+                opacity: 1
+            }, 200);
+
+            $(".menu-icon-line").velocity({
+                backgroundColor: "#FFFFFF"
+            }, 200);
+            container.off("scroll", checkSection2ScrollDown);
+            container.on("scroll", checkSection2ScrollUp);
+        }
+    }
+}
+
+function checkSection2ScrollUp() {
+    if (checkScrollUp) {
+        if (container.scrollTop() < section3Threshold) {
+            sectionFooter = false;
+            checkScrollUp = false;
+            checkScrollDown = true;
+            section3.velocity({
+                opacity: 0
+            }, 200, function() {
+                section3.removeClass("show");
+            });
+            $(".menu-icon-line").velocity({
+                backgroundColor: "#000000"
+            }, 200, function() {
+                $(this).removeAttr("style");
+            });
+
+            container.off("scroll", checkSection2ScrollUp);
+            container.on("scroll", checkSection2ScrollDown);
+        }
+    }
+}
